acfb2b65e8f21d9f957d7e9d350997389b70d77d
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-18 18:10:13 -0400

    fix: fix port declaration

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('website'));
 
 
 // Setup Server
-const port = 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => { console.log(`Server running on port: ${port}`) });
 
 //Routes
